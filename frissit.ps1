e0978f0ff21b73533125ebfba6b63ed9a39252de
Endy07 <endy_311@hotmail.com>
2026-07-04 05:45:36 +0200

    Automatikus feltoltes: frissult az apa weboldalak listaja

--- frissit.ps1
+++ frissit.ps1
@@ -1,7 +1,15 @@
 $hermesPath = "C:\xampp\htdocs\2026\Hermes"
+$apaPath = "C:\xampp\htdocs\2026\Hermes\APA\Eredmenyek"
 $keywords = @("apa", "kárai mihály", "karai mihaly", "karai_mihaly", "kárai_mihály")
 
+if (-not (Test-Path $apaPath)) {
+    New-Item -ItemType Directory -Path $apaPath | Out-Null
+}
+
 $files = Get-ChildItem -Path $hermesPath -Filter *.html -Recurse | Where-Object {
+    if ($_.FullName.StartsWith($apaPath)) { return $false }
+    if ($_.FullName -eq "C:\xampp\htdocs\2026\Hermes\APA\index.html") { return $false }
+
     $name = $_.Name.ToLower()
     $match = $false
     foreach ($kw in $keywords) {
@@ -11,13 +19,26 @@ $files = Get-ChildItem -Path $hermesPath -Filter *.html -Recurse | Where-Object 
         }
     }
     $match
-} | ForEach-Object {
-    # Relatív útvonal a Hermes mappától, + '../' mert az APA mappából nézzük
-    "../" + $_.FullName.Substring($hermesPath.Length + 1).Replace('\', '/')
 }
 
-if ($files) {
-    $files | ConvertTo-Json | Set-Content files.json -Encoding UTF8
+$fileList = @()
+foreach ($file in $files) {
+    # Eredeti almappa struktúra lemásolása
+    $relPath = $file.FullName.Substring($hermesPath.Length + 1)
+    $destFile = Join-Path $apaPath $relPath
+    $destDir = Split-Path $destFile -Parent
+    if (-not (Test-Path $destDir)) {
+        New-Item -ItemType Directory -Path $destDir -Force | Out-Null
+    }
+    Copy-Item -Path $file.FullName -Destination $destFile -Force
+    
+    # Lista építése (HTML számára)
+    $htmlRelPath = "Eredmenyek/" + $relPath.Replace('\', '/')
+    $fileList += $htmlRelPath
+}
+
+if ($fileList) {
+    $fileList | ConvertTo-Json | Set-Content files.json -Encoding UTF8
 } else {
     '[]' | Set-Content files.json -Encoding UTF8
 }
